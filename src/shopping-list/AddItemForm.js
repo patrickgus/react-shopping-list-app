c6a36b1a5f639910a4db42ed631c0fab895d3a6f
Patrick Gustafson <patrickgus@charter.net>
2019-11-26 16:17:50 -0800

    add button functions

--- src/shopping-list/AddItemForm.js
+++ src/shopping-list/AddItemForm.js
@@ -1,17 +1,23 @@
 import React from "react";
 
 export default class AddItemForm extends React.Component {
-    render() {
-      /* TODO: need to wire up the form to add items in App state... */
-      return (
-        <form>
-          <input
-            type='text'
-            placeholder='carrots'
-            aria-label='Shopping list item'
-          />
-          <button type='submit'>Add Item</button>
-        </form>
-      );
-    }
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    // e.target['inputName'] references `<input name='inputName' />`
+    this.props.onAddItem(e.target.itemToAdd.value);
   }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm}>
+        <input
+          name='itemToAdd'
+          type='text'
+          placeholder='carrots'
+          aria-label='Shopping list item'
+        />
+        <button type='submit'>Add Item</button>
+      </form>
+    );
+  }
+}
